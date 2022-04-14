7eae49a1a5f9d19bb02679aeaa8be5ee215ca43b
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-04-14 14:28:44 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const bodyEl = document.querySelector('body');
+
+let modalImage;
+// console.log(galleryItems);
+
+const cardsImages = createGalleryMarkuu(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsImages);
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному 
+// шаблону элемента галереи.
+
+function createGalleryMarkuu(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+
+    // console.log(galleryMarkup)
+}
+console.log(createGalleryMarkuu(galleryItems));
+
+
+// Реализация делегирования на div.gallery и получение url большого изображения.
+galleryContainer.addEventListener('click', onGalleryContainerClick);  
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return
+  }
+evt.target.classList.add('.gallery__image:hover')
+  console.log(evt.target.dataset.source)
+
+}
+
+
+
+
